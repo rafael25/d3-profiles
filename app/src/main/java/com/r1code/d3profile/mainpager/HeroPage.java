256b30a74f50b2ad5db940b3c52419d2444f4548
Rafael Viveros <rafaelvb25@gmail.com>
2015-06-11 00:31:08 -0500

    Add loading spinner

--- app/src/main/java/com/r1code/d3profile/mainpager/HeroPage.java
+++ app/src/main/java/com/r1code/d3profile/mainpager/HeroPage.java
@@ -6,16 +6,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.r1code.d3profile.DataHolder;
+import com.r1code.d3profile.MainActivity;
 import com.r1code.d3profile.R;
 import com.r1code.d3profile.contracts.DataUpdatedHandler;
 import com.r1code.d3profile.events.CurrentHeroIdChanged;
 import com.r1code.d3profile.json.d3hero.Hero;
 import com.r1code.d3profile.services.OttoBusProvider;
 import com.squareup.otto.Subscribe;
+
+import java.text.NumberFormat;
+import java.util.Locale;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -40,6 +46,12 @@ public class HeroPage extends Fragment {
     @InjectView(R.id.hero_big_portrait)
     ImageView heroPortrait;
 
+    @InjectView(R.id.hero_stat_damage)
+    TextView heroDamage;
+
+    @InjectView(R.id.hero_stat_life)
+    TextView heroLife;
+
     private boolean isRegistered = false;
     View view;
 
@@ -48,6 +60,10 @@ public class HeroPage extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.hero_page, container, false);
         ButterKnife.inject(this, view);
+
+        Animation loadAnimation = AnimationUtils.loadAnimation(view.getContext(), R.anim.spinner);
+        heroPortrait.setScaleType(ImageView.ScaleType.CENTER);
+        heroPortrait.startAnimation(loadAnimation);
 
         DataHolder dataHolder = DataHolder.getInstance();
 
@@ -72,11 +88,19 @@ public class HeroPage extends Fragment {
                     String imageName = "@mipmap/" + className.replace('-', '_') + "_" + data.getGender();
                     int portrait = getResources().getIdentifier(imageName, "mipmap", getActivity().getPackageName());
 
+                    NumberFormat nf = NumberFormat.getNumberInstance(Locale.US);
+
                     heroName.setText(data.getName());
                     heroClass.setText(data.getClass_());
                     heroLevel.setText(data.getLevel() + "");
                     paragonLevel.setText(data.getParagonLevel() + "");
+
+                    heroPortrait.setScaleType(ImageView.ScaleType.CENTER_CROP);
                     heroPortrait.setImageResource(portrait);
+                    heroPortrait.clearAnimation();
+
+                    heroDamage.setText(nf.format(data.getStats().getDamage()));
+                    heroLife.setText(nf.format(data.getStats().getLife()));
                 }
             }
         });
